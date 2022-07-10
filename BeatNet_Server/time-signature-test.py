6cb1c96dc706f671670fd6d607fdc025aa899cfa
terry <tzfeng@ucsd.edu>
2022-07-09 21:45:43 -0700

    BeatNet server bpm endpoint complete

--- BeatNet_Server/time-signature-test.py
+++ BeatNet_Server/time-signature-test.py
@@ -5,8 +5,9 @@ filename = "BeatNet_Model.ml"
 
 estimator = BeatNet(1, mode='offline', inference_model='DBN', plot=[], thread=False)
 
+# Export BeatNet ML Model
 #pickle.dump(estimator, open(filename,"wb"))
 
-Output = estimator.process("https://www2.cs.uic.edu/~i101/SoundFiles/StarWars3.wav")
+Output = estimator.process("./Teakwood.wav")
 
 print(Output)
