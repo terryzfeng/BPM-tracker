80ce8c9bd28afd4a6f41fb1d0ca8a96ac3911363
terry <tzfeng@ucsd.edu>
2022-07-09 22:57:01 -0700

    check file upload

--- player.js
+++ player.js
@@ -2,27 +2,26 @@ let url = "";
 let urlLoaded = false;
 
 const getValuesFromInputs = () => {
-  const profileAud = document.querySelector('input.profile-aud').files[0];
+  const profileAud = document.querySelector("input.profile-aud").files[0];
   //document.querySelector('form').style.display = 'none';
   return [profileAud];
-}
+};
 
 const convertInputValues = () => {
   const [profileAud] = getValuesFromInputs();
   const profileAudURL = URL.createObjectURL(profileAud);
-  return [profileAudURL]
-}
+  converted = true;
+  return [profileAudURL];
+};
 
 const addInputToProfile = () => {
   const [profileAudURL] = convertInputValues();
-  document.querySelector('.aud').setAttribute('src', profileAudURL);
+  document.querySelector(".aud").setAttribute("src", profileAudURL);
   url = profileAudURL;
   urlLoaded = true;
-  //sound.play();
-}
+};
 
-document.querySelector('button').addEventListener('click', (e) => {
+document.querySelector("button").addEventListener("click", (e) => {
   e.preventDefault();
   addInputToProfile();
 });
-
