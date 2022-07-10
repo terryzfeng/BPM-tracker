025feceae11a5895afa05ad690ea953bfb7dfaf9
terry <tzfeng@ucsd.edu>
2022-07-10 02:57:55 -0700

    give up on python communication, html request is bad

--- utils.js
+++ utils.js
@@ -1,56 +1,55 @@
 // BeatNet Server
-const serverURL = 'http://127.0.0.1:9099/bpm';
-const testURL = 'http://127.0.0.1:9099/test';
+const serverURL = "http://127.0.0.1:9099/bpm";
+const testURL = "http://127.0.0.1:9099/test";
 
 function millisToBPM(millis) {
-    let bpm = 60000.0 / millis;
+  let bpm = 60000.0 / millis;
 
-    // Keep BPM guess within 60bpm to 160bpm
-    if (bpm < 60) {
-        bpm *= 2;
-    } else if (bpm > 160) {
-        bpm /= 2;
-    }
+  // Keep BPM guess within 60bpm to 160bpm
+  if (bpm < 60) {
+    bpm *= 2;
+  } else if (bpm > 160) {
+    bpm /= 2;
+  }
 
-    return Math.round(bpm);
+  return Math.round(bpm);
 }
 
-let im;
+let response;
 function blobUpload() {
-    if (song != null && song.isLoaded()) {
-        print("Loaded: 100%, Uploading...");
-        let soundBlob = song.getBlob();
-        let formdata = new FormData();
-        formdata.append('soundBlob', soundBlob, 'audio.wav');
-        // Create POST Request to 
-        let httpsRequestOptions = {
-            method: "POST",
-            data: formdata,
-            headers: new Headers({
-                'Content-Type': 'multipart/form-data'
-            }),
-            mode: 'no-cors'
-        };
-        // Upload
-        httpDo( serverURL, httpsRequestOptions, 
-            function (response) { 
-                print("success")
-                print(response.data) 
-            },
-            function (error) {
-                print("fail")
-                print(error)
-            })
-        //httpDo('https://dog.ceo/api/breeds/image/random', "GET", function(response) {print(response)})
-        //httpDo(testURL, "POST", function(response) {print(response)})
-    }
-}
+  if (song != null && song.isLoaded()) {
+    print("Loaded: 100%, Uploading...");
+    let soundBlob = song.getBlob();
+    let formdata = new FormData();
+    formdata.append("soundBlob", soundBlob, "audio.wav");
+    // Create POST Request to
+    let httpsRequestOptions = {
+      method: "POST",
+      datatype: "json",
+      data: formdata,
+      headers: new Headers({
+        "Content-Type": "multipart/form-data",
+        "Accept": "application/json",
+      }),
+      observe: "response",
+      mode: "no-cors",
+    };
+    // Upload
+    httpDo(serverURL, httpsRequestOptions, (response) => {
+      if (response.status == 200) {
+        print("ok");
+      }
+    });
 
+    //httpDo( "https://dog.ceo/api/breeds/image/random", "GET", function (response) { print(response); });
+    //httpDo( "https://randomuser.me/api/", { method: "GET", datatype: "json" }, function (response) { print(response.status); }, (response) => { print("fail"); });
+  }
+}
 
 function error() {
-    print("ERROR - BAD UPLOAD")
+  print("ERROR - BAD UPLOAD");
 }
 
 function loading(num) {
-    print("LOADING: " + Math.round(num) + "%");
+  print("LOADING: " + Math.round(num) + "%");
 }
